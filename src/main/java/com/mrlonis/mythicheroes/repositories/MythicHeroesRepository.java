6f3002381ac1ebd5149572ac28c2bcf8af09b26f
Matthew L <mattlonis@gmail.com>
2021-11-01 17:42:42 -0400

    Update MythicHeroesRepository.java

--- src/main/java/com/mrlonis/mythicheroes/repositories/MythicHeroesRepository.java
+++ src/main/java/com/mrlonis/mythicheroes/repositories/MythicHeroesRepository.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.UUID;
 
-@RepositoryRestResource(collectionResourceRel = "mythicHeroes", path = "mythicHeroes")
+@RepositoryRestResource(collectionResourceRel = "data", path = "mythicHeroes")
 @CrossOrigin(origins = "http://localhost:4200")
 public interface MythicHeroesRepository extends PagingAndSortingRepository<MythicHeroes, UUID> {
     List<MythicHeroes> findAll();
