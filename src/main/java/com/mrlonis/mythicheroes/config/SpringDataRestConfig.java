59ee82853880c390ba59dac4e8d85832711a4d1f
Matthew L <mattlonis@gmail.com>
2021-11-01 01:01:24 -0400

    Added cors for angular demo and exposing ids for entities through new data rest config

---
+++ src/main/java/com/mrlonis/mythicheroes/config/SpringDataRestConfig.java
@@ -0,0 +1,22 @@
+package com.mrlonis.mythicheroes.config;
+
+import com.mrlonis.mythicheroes.entities.Faction;
+import com.mrlonis.mythicheroes.entities.MythicHeroes;
+import com.mrlonis.mythicheroes.entities.Rarity;
+import com.mrlonis.mythicheroes.entities.Type;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Component
+public class SpringDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(MythicHeroes.class);
+        config.exposeIdsFor(Faction.class);
+        config.exposeIdsFor(Rarity.class);
+        config.exposeIdsFor(Type.class);
+
+    }
+}
--- src/main/java/com/mrlonis/mythicheroes/repositories/MythicHeroesRepository.java
+++ src/main/java/com/mrlonis/mythicheroes/repositories/MythicHeroesRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "mythicHeroes", path = "mythicHeroes")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface MythicHeroesRepository extends PagingAndSortingRepository<MythicHeroes, UUID> {
     List<MythicHeroes> findAll();
 
