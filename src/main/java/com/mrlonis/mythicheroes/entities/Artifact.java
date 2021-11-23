79e8eec418fd5dd2b18bec59b797c4ffeda84329
Matthew L <mattlonis@gmail.com>
2021-11-23 11:38:07 -0500

    Create Artifact.java

---
+++ src/main/java/com/mrlonis/mythicheroes/entities/Artifact.java
@@ -0,0 +1,32 @@
+package com.mrlonis.mythicheroes.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "MythicHeroes_Artifacts")
+public class Artifact implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+}
