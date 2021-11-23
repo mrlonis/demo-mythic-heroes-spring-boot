d7c77eeb981dc531e5283d986ea643ffebd96389
Matthew L <mattlonis@gmail.com>
2021-11-23 10:21:53 -0500

    Update TypeRepository.java

--- src/main/java/com/mrlonis/mythicheroes/repositories/TypeRepository.java
+++ src/main/java/com/mrlonis/mythicheroes/repositories/TypeRepository.java
@@ -4,6 +4,7 @@ import com.mrlonis.mythicheroes.entities.Type;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
@@ -14,5 +15,6 @@ import java.util.UUID;
 public interface TypeRepository extends PagingAndSortingRepository<Type, UUID> {
     List<Type> findAll();
 
-    List<Type> findByName(@Param("name") String name);
+    @RestResource(path = "findBy", rel = "findBy")
+    List<Type> findByNameIgnoreCaseContains(@Param("name") String name);
 }
