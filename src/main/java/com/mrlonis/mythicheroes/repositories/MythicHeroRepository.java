beab1690b38f64ca1af334d53d4d772ddbba8af4
Matthew L <mattlonis@gmail.com>
2021-11-23 10:21:47 -0500

    Update MythicHeroRepository.java

--- src/main/java/com/mrlonis/mythicheroes/repositories/MythicHeroRepository.java
+++ src/main/java/com/mrlonis/mythicheroes/repositories/MythicHeroRepository.java
@@ -1,18 +1,26 @@
 package com.mrlonis.mythicheroes.repositories;
 
 import com.mrlonis.mythicheroes.entities.MythicHero;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "data", itemResourceRel = "item", path = "mythicHero")
 @CrossOrigin(origins = "http://localhost:4200")
 public interface MythicHeroRepository extends PagingAndSortingRepository<MythicHero, UUID> {
-    List<MythicHero> findAll();
+    Page<MythicHero> findAll(Pageable pageable);
 
-    List<MythicHero> findByName(@Param("name") String name);
+    @RestResource(path = "findBy", rel = "findBy")
+    Page<MythicHero> findByNameIgnoreCaseContainsAndFaction_NameIgnoreCaseContainsAndRarity_NameIgnoreCaseContainsAndType_NameIgnoreCaseContains(
+            @Param("name") String name,
+            @Param("factionName") String factionName,
+            @Param("rarityName") String rarityName,
+            @Param("typeName") String typeName,
+            Pageable pageable);
 }
