6313644a3113df9047817463cf08d89c2aea126b
Matthew L <mattlonis@gmail.com>
2022-04-25 00:32:55 -0400

    Updating project dependencies

--- src/main/java/com/mrlonis/mythicheroes/repositories/RarityRepository.java
+++ src/main/java/com/mrlonis/mythicheroes/repositories/RarityRepository.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.UUID;
 
-@RepositoryRestResource(collectionResourceRel = "data", itemResourceRel = "item", path = "rarity")
+@RepositoryRestResource(collectionResourceRel = "data", itemResourceRel = "item", path = "rarity", exported = false)
 @CrossOrigin(origins = "http://localhost:4200")
 public interface RarityRepository extends PagingAndSortingRepository<Rarity, UUID> {
     List<Rarity> findAll();
--- src/main/java/com/mrlonis/mythicheroes/repositories/TypeRepository.java
+++ src/main/java/com/mrlonis/mythicheroes/repositories/TypeRepository.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.UUID;
 
-@RepositoryRestResource(collectionResourceRel = "data", itemResourceRel = "item", path = "type")
+@RepositoryRestResource(collectionResourceRel = "data", itemResourceRel = "item", path = "type", exported = false)
 @CrossOrigin(origins = "http://localhost:4200")
 public interface TypeRepository extends PagingAndSortingRepository<Type, UUID> {
     List<Type> findAll();
